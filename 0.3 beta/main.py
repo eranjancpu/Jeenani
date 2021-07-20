f1ce55902e539d6088afe40f59eaf62c35185d4a
Jeenathcpu <74848678+eranjancpu@users.noreply.github.com>
2021-07-20 08:14:39 +0530

    Add files via upload

---
+++ 0.3 beta/main.py
@@ -0,0 +1,55 @@
+import commands
+import pyttsx3
+
+engine = pyttsx3.init("sapi5")
+engine.setProperty("rate", 150)
+voice = engine.getProperty("voices")
+engine.setProperty('voice', voice[0].id)
+
+outMath = "answer is "
+
+def speak(msg):
+    engine.say(msg)
+    engine.runAndWait()
+
+DIGITS = ["1","2","3","4","5","6","7","8","9","0"]
+
+print("Jeenani assistant version 0.3 Beta")
+
+while True:
+    comm = input("")
+    assistant = commands.talkBack()
+
+    if(comm == "math" or comm == "Math"):
+        speak("   first number: ")
+        nu1 = input("   first number ")
+        speak("   operator ")
+        oppo = input("   operator: ")
+        speak("   second number ")
+        nu2 = input("   second number: ")
+
+        try:
+            nu1 = int(nu1)
+            nu2 = int(nu2)
+            if(oppo == '+'):
+                print(nu1 + nu2)
+                speak(f"{outMath}{nu1 + nu2}")
+            elif(oppo == '-'):
+                print(nu1 - nu2)
+                speak(f"{outMath},{nu1 - nu2}")
+            elif(oppo == '*' or oppo == 'x'):
+                print(nu1 * nu2)
+                speak(f"{outMath},{nu1 * nu2}")
+            elif(oppo == '/'):
+                print(nu1 / nu2)
+                speak(f"{outMath}{nu1 / nu2}")
+            else:
+                speak("the operator is invalid")
+        except:
+            speak("given number is invalid")
+
+    elif(comm == "quit"): break
+    else: speak(assistant.talk(comm))
+
+speak("bye sir/miss")    
+    
---
+++ 0.4.0 beta/commands.py
@@ -0,0 +1,86 @@
+import webbrowser
+import pyttsx3
+
+enginei = pyttsx3.init("sapi5")
+enginei.setProperty("rate", 150)
+voice = enginei.getProperty("voices")
+enginei.setProperty('voice', voice[1].id)
+
+def speak(msg):
+    enginei.say(msg)
+    enginei.runAndWait()
+
+class talkBack:
+    def __init__(self):
+
+        # for inputs
+        self.HAYou = ["How are you", "how are you", "How are yo", "how are yo", "what's up", "whaUp"]
+        self.IAfine = ["I am fine", "i am fine", "I am good", "i am good"]
+        self.HDYMade = ["How do you made in", "how do you made in", "What language do you made", "What contry do you made in", "what contry do you made in"]
+        self.GTWeb = ["go to the website:", "Go to the website:", "go to web:", "Go to web:"]
+        self.hello = ["hello", "Hello", "hi", "Hi", "HI"]
+        self.amaz = ["wow", "Wow", "WoW", "WOW"]
+
+        # for outputs 
+        self.talkHowAreYou = "I am fine working for 1000 people for the whole world"
+        self.talkWhatUp = "Nothing here, just working to 1000 people including sir/miss"
+        self.talkFine = "Wow, thats good"
+        self.talkMade = "I am made in python"
+        self.talkMadeContry = "I am made in sri lanka"
+        self.GTWout = "ok sir/miss"
+        self.noUnder = "sorry, can you please tell me again"
+        self.GTWreturn = "What website do sir/miss want to go? "
+        self.HTalk = "Hello, I am Jeenani"
+        self.amazOut = "thank you"
+    
+    def talk(self, com):
+        # for HAYou
+        if(com == self.HAYou[0]): return self.talkHowAreYou
+        elif(com == self.HAYou[1]): return self.talkHowAreYou
+        elif(com == self.HAYou[2]): return self.talkHowAreYou
+        elif(com == self.HAYou[3]): return self.talkHowAreYou
+        elif(com == self.HAYou[4]): return self.talkWhatUp
+        elif(com == self.HAYou[5]): return self.talkWhatUp        
+        elif(com == self.IAfine[0]): return self.talkFine # for IAFine
+        elif(com == self.IAfine[1]): return self.talkFine
+        elif(com == self.IAfine[2]): return self.talkFine
+        elif(com == self.IAfine[3]): return self.talkFine
+        elif(com == self.HDYMade[0]): return self.talkMade # for HDYMade
+        elif(com == self.HDYMade[1]): return self.talkMade
+        elif(com == self.HDYMade[2]): return self.talkMade
+        elif(com == self.HDYMade[3]): return self.talkMadeContry
+        elif(com == self.HDYMade[4]): return self.talkMadeContry
+        elif(com == self.GTWeb[0]):  # for GTWeb
+            speak(self.GTWreturn)
+            web = input(self.GTWreturn)
+            webbrowser.open(web)
+            return self.GTWout
+        elif(com == self.GTWeb[1]):
+            speak(self.GTWreturn)
+            web = input(self.GTWreturn)
+            webbrowser.open(web)
+            return self.GTWout
+        elif(com == self.GTWeb[2]):
+            speak(self.GTWreturn)
+            web = input(self.GTWreturn)
+            webbrowser.open(web)
+            return self.GTWout
+        elif(com == self.GTWeb[3]):
+            speak(self.GTWreturn)
+            web = input(self.GTWreturn)
+            webbrowser.open(web)
+            return self.GTWout
+        elif(com == self.hello[0]): return self.HTalk # for hello
+        elif(com == self.hello[1]): return self.HTalk
+        elif(com == self.hello[2]): return self.HTalk
+        elif(com == self.hello[3]): return self.HTalk
+        elif(com == self.hello[0]): return self.HTalk
+        elif(com == self.hello[1]): return self.HTalk
+        elif(com == self.hello[2]): return self.HTalk
+        elif(com == self.hello[3]): return self.HTalk
+        elif(com == self.amaz[0]): return self.amazOut
+        elif(com == self.amaz[1]): return self.amazOut
+        elif(com == self.amaz[2]): return self.amazOut
+        elif(com == self.amaz[3]): return self.amazOut
+        else: return self.noUnder
+        
